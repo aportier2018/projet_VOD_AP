3d227256cd34cbf17a667667fd2393309e93bf75
aportier2018 <anne.portier@wanadoo.fr>
2018-07-03 11:29:29 +0200

    3 juillet 11h28 mis en place du github

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+  <head>
+  	<meta charset="utf-8">
+  	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+  	<title>Projet VOD AP</title>
+  	<link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+
+  </body>
+</html>
